d9046dfe21b4eacd7f063ff1fbbb32a11673b92a
emilsberzs <70856334+emilsberzs@users.noreply.github.com>
2020-12-27 16:45:27 +0000

    Created Subject, Course and Module models

--- educa/settings.py
+++ educa/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'course.apps.CoursesConfig',
+    'courses.apps.CoursesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
