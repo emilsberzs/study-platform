57da8fe786b9bd8140ea7264a4fecc5d29e815ee
emilsberzs <70856334+emilsberzs@users.noreply.github.com>
2020-12-27 19:01:10 +0000

    Custom model fields for ordering

---
+++ courses/fields.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
+
+
+class OrderField(models.PositiveIntegerField):
+    def __init__(self, for_fields=None, *args, **kwargs):
+        self.for_fields = for_fields
+        super().__init__(*args, **kwargs)
+
+    def pre_save(self, model_instane, add):
+        if getattr(model_instane, self.attname) is None:
+            # no current value
+            try:
+                qs = self.model.objects.all()
+                if self.for_fields:
+                    # filter by objects with the same field values
+                    # for the field in "for_fields"
+                    query = {field: getattr(model_instane, field)
+                             for field in self.for_fields}
+                    qs = qs.filter(**query)
+                # get the order of the last item
+                last_item = qs.latest(self.attname)
+                value = last_item.order + 1
+            except ObjectDoesNotExist:
+                value = 0
+            setattr(model_instane, self.attname, value)
+            return value
+        else:
+            return super().pre_save(model_instane, add)
--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User  # pylint: disable=imported-auth-user
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 
 class Subject(models.Model):
@@ -21,8 +24,8 @@ class Course(models.Model):
                                 related_name='courses',
                                 on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    slusg = models.SlugField(max_length=200, unique=True)
-    overview = models.TimeField()
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -38,6 +41,60 @@ class Module(models.Model):
                                on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return f'{self.order}.{self.title}'
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module,
+                               related_name='contents',
+                               on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,
+                                     on_delete=models.CASCADE,
+                                     limit_choices_to={'model__in': (
+                                         'text',
+                                         'video',
+                                         'image',
+                                         'file')})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User,
+                              related_name='%(class)s_related',
+                              on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
 
     def __str__(self):
         return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    content = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+    content = models.FileField(upload_to='images')
+
+
+class Video(ItemBase):
+    url = models.URLField()
