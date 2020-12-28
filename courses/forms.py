37541c57d4624683ef259b7aee3bd82344686643
emilsberzs <70856334+emilsberzs@users.noreply.github.com>
2020-12-28 16:53:14 +0000

    Course module formsets in forms.py and corresponding template

---
+++ courses/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.forms.models import inlineformset_factory
+from .models import Module, Course
+
+ModuleFormSet = inlineformset_factory(Course,
+                                      Module,
+                                      fields=['title', 'description'],
+                                      extra=2,
+                                      can_delete=True)
