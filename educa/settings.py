41ff2cb5bffd1836bf85e7e8fd9ffd1fff7b4c95
emilsberzs <70856334+emilsberzs@users.noreply.github.com>
2021-01-07 19:37:13 +0000

    Added Redis channel layer

--- educa/settings.py
+++ educa/settings.py
@@ -160,3 +160,13 @@ REST_FRAMEWORK = {
     ],
 
 }
+
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
